ac1bb2c35502a16700da298fdceaa807330041c5
Jabs081 <abassjohn37@gmail.com>
2025-05-26 18:20:25 +0100

    react application vlite version implementation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Jabs Portfolio</h1>
+      <p>This is a modernized Vite + React setup.</p>
+    </div>
+  )
+}
+
+export default App
